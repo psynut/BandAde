e7aae822208bf37460b5b029c0058c3fdb74b7cb
Michael Sibrava <48391846+psynut@users.noreply.github.com>
2022-11-07 15:36:34 -0800

    Stuck with trees
    --Added pictures of characters.
    Added methods for TreeScript to identify other dead trees next to them. They are not all catching on fire, however, when I light them on fire.

--- Assets/TreeScript.cs
+++ Assets/TreeScript.cs
@@ -8,9 +8,12 @@ public class TreeScript : MonoBehaviour
     public GameObject[] foliage;
     public Transform[] transforms; //0. Tree Transform; 1. Foliage Transform
 
+    private TreeScript[] deadTreeNieghbors;
+
     public bool alive = true;
 
-    private bool onFire = false;
+    [HideInInspector]
+    public bool onFire = false;
     private ParticleSystem particleSystem;
 
     private void OnDrawGizmos() {
@@ -34,8 +37,11 @@ public class TreeScript : MonoBehaviour
         if(alive) {
             GameObject leaves = GameObject.Instantiate(foliage[Random.Range(0,trunks.Length)],transforms[1].position,Quaternion.Euler(-90,0,0),transforms[1]);
             leaves.transform.name = "Foliage";
+        } else {
+            deadTreeNieghbors = FindNeighborDeadTrees();
         }
         particleSystem = GetComponent<ParticleSystem>();
+        
     }
 
     // Update is called once per frame
@@ -53,7 +59,38 @@ public class TreeScript : MonoBehaviour
             
     }
 
-    private void CatchFire() {
+    public void CatchFire() {
+        onFire = true;
         particleSystem.Play();
+        foreach(TreeScript treeScript in deadTreeNieghbors) {
+            if(treeScript.onFire == false) {
+                treeScript.CatchFire();
+            }
+        }
+    }
+
+    private TreeScript[] FindNeighborDeadTrees() {
+        BoxCollider boxCollider = GetComponent<BoxCollider>();
+        if(boxCollider == null) {
+            Debug.LogWarning("No boxcollider found in this Tree gameojbect at: " + gameObject.transform.position);
+        }
+        //Ideally trees are cube or squares -- trying to make this more idiot proof.
+        float[] treeSizes = {boxCollider.size.z * transform.lossyScale.z, boxCollider.size.x * transform.lossyScale.x };
+        if(treeSizes[0] != treeSizes[1]) {
+            Debug.LogWarning("Tree size is not uniform on x & z axis - using x axis scale.");
+        }
+
+        Vector3[] cardinalDirection = { Vector3.forward,Vector3.right,Vector3.back,Vector3.left };
+        List<TreeScript> trees = new List<TreeScript>();
+        for(int i = 0; i < cardinalDirection.Length; i++) {
+            Physics.Raycast(transform.position,cardinalDirection[i],out RaycastHit hit, treeSizes[i % 2]);
+            if(hit.transform) {
+                TreeScript m_treeScript = hit.transform.GetComponent<TreeScript>();
+                if(m_treeScript != null && hit.transform.GetComponent<TreeScript>().alive == false) {
+                    trees.Add(hit.transform.GetComponent<TreeScript>());
+                }
+            }
+        }
+        return trees.ToArray();
     }
 }
